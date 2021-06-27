9174aae68e863e7df87643f485352405bf53bd5d
WilliDoo <85503883+WilliDoo@users.noreply.github.com>
2021-06-27 14:42:08 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,10 @@
 import vex
+# Configured devices
+# Controller/ Name: Controller1/ config: LA directions RA turn zr/ zl claw  
+# Drivetrain/ Name: drivetrain/ config: 4 motor Mecanum drivetrain, offers more mobility but slightly less speed because 
+# Motor Group/ Name: claw/ config: REV RM, FOR LM
+
+while True:
+  if Controller1.axis1 != 0 or Controller1.axis2 != 0:
+    wait(20, MM)
+   
